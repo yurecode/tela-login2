bdeaf17f344e45edcd882d445ebf727a9c96dd0c
Yure <yurecod@gmail.com>
2024-05-14 12:08:57 -0400

    criacao de cadastro

--- script.js
+++ script.js
@@ -49,3 +49,54 @@ document.addEventListener('DOMContentLoaded', () => {
     // Você pode usar o token armazenado para fazer requisições autenticadas
   }
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+  const registerForm = document.querySelector('#container .sign-up form')
+
+  registerForm.addEventListener('submit', async (event) => {
+    event.preventDefault()
+
+    const name = document.querySelector(
+      '#container .sign-up input[type="text"]'
+    ).value
+    const email = document.querySelector(
+      '#container .sign-up input[type="email"]'
+    ).value
+    const password = document.querySelector(
+      '#container .sign-up input[type="password"]'
+    ).value
+
+    const userData = {
+      name,
+      email,
+      password,
+    }
+
+    try {
+      // Fazer requisição POST para cadastrar o usuário
+      const response = await axios.post('http://localhost:3006/user', userData)
+
+      // Se o cadastro for bem-sucedido, você pode exibir uma mensagem ou redirecionar para a página de login
+      console.log('Usuário cadastrado com sucesso:', response.data)
+
+      // Redirecionar para o formulário de login após o cadastro
+      container.classList.remove('active')
+    } catch (error) {
+      alert(error.response.data.message)
+      console.error('Erro ao cadastrar usuário:', error.response.data.message)
+      // Mostrar mensagem de erro na interface do usuário
+    }
+  })
+
+  // Verificar se há um token armazenado no localStorage ao carregar a página
+  const storedToken = localStorage.getItem('token')
+  if (storedToken) {
+    console.log('Token JWT armazenado:', storedToken)
+    // Você pode usar o token armazenado para fazer requisições autenticadas
+  }
+
+  const loginBtn = document.getElementById('login')
+  loginBtn.addEventListener('click', () => {
+    container.classList.remove('active')
+  })
+})
